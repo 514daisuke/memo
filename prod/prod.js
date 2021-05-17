b1c5f387ee4ccf18bb9ff33ae3ac5028194b7c9c
kosuke514 <29258636+514daisuke@users.noreply.github.com>
2021-05-17 23:53:23 +0900

    デフォルトメモの追加

--- prod/prod.js
+++ prod/prod.js
@@ -4,21 +4,67 @@
 const addBtn = document.getElementById('add')
 //クリックされた回数をカウント
 let count = 0;
+let num = 0;
+//メモの数のカウント
 function counter(){
     count++;
     console.log(count);
+    let num = count;
 }
-    console.log("------------");
-    console.log(count);
+    console.log(num);
+
+// 日付の操作
+let date = new Date();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let day = date.getDate();
+
+document.getElementById("today").innerHTML = year + "年" + month + "月" + day + "日のメモ";
+
 
 //ローカルストレージからデータの取得
 const memos = JSON.parse(localStorage.getItem('memos'))
 
 // // メモ帳追加処理を実行 addNewMemo(memo)の処理
-for (let i = 0; i < 3; i++){
 if (memos) {
     memos.forEach(memo => addNewMemo(memo))
 }
+
+
+//デフォルトメモの処理
+function NewMemo(text = '') {
+    // 操作要素作成
+    //編集
+    const editBtn = memo.querySelector('.edit')
+    //削除
+    const deleteBtn = memo.querySelector('.delete')
+    //メイン
+    const main = memo.querySelector('.main')
+    //テキストエリア
+    const textArea = memo.querySelector('textarea')
+
+    // テキストエリアに引数で渡したテキストを代入（新規/編集）
+    textArea.value = text
+    // markedは、HTMLに追加したプラグイン
+    main.innerHTML = marked(text)
+
+    // 削除クリックイベント
+    deleteBtn.addEventListener('click', () => {
+        deleteMemo(memo)
+    })
+
+    // // 編集ボタンのクリックイベント
+    editBtn.addEventListener('click', () => {
+        editMemo(main, textArea)
+    })
+
+    // テキストエリアのイベント
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target
+        main.innerHTML = marked(value)
+        // ローカルストレージの更新
+        updateLS()
+    })
 }
 
 //addボタンをクリックした場合の作成イベント
@@ -40,6 +86,7 @@ function addNewMemo(text = '') {
     <div class="main ${text ? "" : "hidden"}"></div>
     <textarea class="${text ? "hidden" : ""}"></textarea>
     `
+
 
     // 操作要素作成
     //編集
@@ -76,7 +123,6 @@ function addNewMemo(text = '') {
         updateLS()
 
     })
-
     // bodyの子要素として追加
     document.body.appendChild(memo)
 }
@@ -94,7 +140,6 @@ function updateLS() {
 
     // ローカルストレージを保存（memo）
     localStorage.setItem('memos', JSON.stringify(memos))
-
 }
 
 // メモ帳削除
